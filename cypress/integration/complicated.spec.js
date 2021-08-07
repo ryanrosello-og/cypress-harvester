cf4fe7d9de30dd842782e76a727e59fb7e091c32
ryanrosello-og <goneby30@gmail.com>
2021-08-08 07:14:16 +1000

    added fixture/clean up

--- cypress/integration/complicated.spec.js
+++ cypress/integration/complicated.spec.js
@@ -1,42 +1,41 @@
 context('repeating elements', () => {
   beforeEach(() => {
-    cy.visit('https://docs.cypress.io/plugins/directory');
+    cy.visit('./cypress/fixtures/test_html_repeating_elements.html');
   });
 
   it('scrape', () => {
     cy.contains('h1', 'Plugins').should('be.visible');
     const suppressLog = { log: false }
     const parentLocator = 'li.m-3';
-    const elems = [
+    const elementsToScrape = [
       { label: 'name', locator: 'h3' },
       { label: 'description', locator: 'p' },
       { label: 'keywords', locator: '.break-words' },
       { label: 'badge', locator: '[class*=pluginBadge]' },
     ];
     
-    let res = [];
+    let data = [];
     cy.get(parentLocator, suppressLog).each(($el, index, $list) => {
       cy.wrap($el, suppressLog).then(($body) => {
-        let obj = {};
-        cy.wrap(elems, suppressLog).each(($e, i, $elms) => {
-          let loc = elems[i].locator;
-          if ($body.find(loc).length > 0) {
-            cy.get($el, suppressLog).within(() => {
-              cy.get(loc, suppressLog)
-                .invoke('text')
+        let scrappedObject = {};
+        cy.wrap(elementsToScrape, suppressLog).each(($e, i, $elms) => {
+          let childElementLocator = elementsToScrape[i].locator;
+          if ($body.find(childElementLocator).length > 0) {
+            cy.get($el, suppressLog).within(suppressLog, () => {
+              cy.get(childElementLocator, suppressLog)
+                .invoke(suppressLog, 'text')
                 .then((txt) => {
-                  obj[elems[i].label] = txt;
+                  scrappedObject[elementsToScrape[i].label] = txt;
                 });
-              
             });
           } else {
-            obj[elems[i].label] = '';
+            scrappedObject[elementsToScrape[i].label] = '';
           }
         });
 
-        res.push(obj);
+        data.push(scrappedObject);
       });
     });
-    cy.log(res);
+    cy.log(data);
   });
 });
