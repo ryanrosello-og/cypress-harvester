b96126e88aa13ba4d93571cd7de4b4b3e737c802
ryanrosello-og <goneby30@gmail.com>
2021-11-02 18:56:10 +1000

    fix failing test

--- cypress/integration/date-columns.spec.js
+++ cypress/integration/date-columns.spec.js
@@ -64,19 +64,19 @@ context('Harvester', () => {
       .then((scrapedData) => {
         expect(scrapedData.data).to.deep.eq([
           {
-            created: 1633354817,
+            created: 1633354817000,
             modified: '2012-10-11',
             invalid_date: '15-May-2011 10:20AM',
             deleted: '15-May-2011 10:20 AM',
           },
           {
-            created: 1633441217,
+            created: 1633441217000,
             modified: '2012-10-10',
             invalid_date: '13-May-2011 8:15PM',
             deleted: '13-May-2011 8:15 PM',
           },
           {
-            created: 1633527617,
+            created: 1633527617000,
             modified: '2012-10-09',
             invalid_date: '17-May-2011 9:20AM',
             deleted: '17-May-2011 9:20 AM',
